5c70444b30ff2a53f608bd74c50fe9b0fc65784e
Lewis B CIC <lewis.bailey-cic@ibm.com>
2022-08-17 16:47:49 +0100

    added multiple pages and improved UI for phase banner

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,18 +1,19 @@
-function ready (fn) {
+function ready(fn) {
   if (document.readyState !== 'loading') {
     // IE9 support
-    fn()
+    fn();
   } else {
     // Everything else
-    document.addEventListener('DOMContentLoaded', fn)
+    document.addEventListener('DOMContentLoaded', fn);
   }
 }
 
 // Warn about using the kit in production
 if (window.console && window.console.info) {
-  window.console.info('GOV.UK Prototype Kit - do not use for production')
+  window.console.info('GOV.UK Prototype Kit - do not use for production');
 }
 
 ready(() => {
-  window.GOVUKFrontend.initAll()
-})
+  window.GOVUKFrontend.initAll();
+  window.MOJFrontend.initAll();
+});
