551acd6ac21a65ef9fbbf488c206bf0fd8601094
James B <j.bowler@reply.com>
2023-03-22 09:21:22 +0000

    Update results view to match new design

--- app/config.js
+++ app/config.js
@@ -4,24 +4,24 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'DBS Online Result',
+  serviceName: "Disclosure and Barring Service Online Result",
 
   // Default port that prototype runs on
-  port: '4000',
+  port: "4000",
 
   // Enable or disable password protection on production
-  useAuth: 'true',
+  useAuth: "true",
 
   // Automatically stores form data, and send to all views
-  useAutoStoreData: 'true',
+  useAutoStoreData: "true",
 
   // Enable cookie-based session store (persists on restart)
   // Please note 4KB cookie limit per domain, cookies too large will silently be ignored
-  useCookieSessionStore: 'false',
+  useCookieSessionStore: "false",
 
   // Force HTTP to redirect to HTTPS on production
-  useHttps: 'true',
+  useHttps: "true",
 
   // Enable or disable Browser Sync (local development only)
-  useBrowserSync: 'true',
+  useBrowserSync: "true",
 };
